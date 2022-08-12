a47a90cff0349b579b30c9c343b9490ad46fb769
M&B Code <105244471+Bountukp20@users.noreply.github.com>
2022-08-12 09:33:43 -0500

    Update urls.py

--- django_auth/django_project/accounts/urls.py
+++ django_auth/django_project/accounts/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
+from django.urls import path, include
 
-from .views import SignUpView, homepage, library, login, about
+from .views import SignUpView, homepage, library, login, main2, download, saved, account, home2, password_reset, password_complete, password_confirm, password_done, password_form
 
 
 urlpatterns = [
@@ -8,5 +8,14 @@ urlpatterns = [
     path('home/', homepage, name = 'home'),
     path('library', library, name = 'library'),
     path('login', login, name = 'login'),
-    path('about/<int:id>', about, name = 'about'),
+    path('main2', main2, name = 'main2'),
+    path('download', download, name = 'download'),
+    path('saved', saved, name = 'saved'),
+    path('account', account, name = 'account'),
+    path('home2', home2, name="home2"),
+    path('password_reset', password_reset, name="password_reset"),
+    path('password_complete', password_complete, name="password_complete"),
+    path(' password_confirm', password_confirm, name=" password_confirm"),
+    path('password_done', password_done, name="password_done"),
+    path('password_form', password_form, name="password_form"),
 ]
